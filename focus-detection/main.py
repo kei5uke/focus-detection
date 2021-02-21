50f6839fade6c20b687cac4a7e5a3c0bf9463601
keisuke-jpn <keiske.message@gmail.com>
2021-02-21 16:00:43 +0200

    remade main.py

--- focus-detection/main.py
+++ focus-detection/main.py
@@ -21,45 +21,44 @@ class MeasureGSR:
         filename = input("Name CSV File: ")+'.csv'
         self.file = open(filename, 'w')
         self.writer=csv.writer(self.file,lineterminator='\n')
-        self.writer.writerow(['GSR','STATE','SEC'])
+        self.writer.writerow(['SEC','GSR','STATE'])
 
-    def measure(self, state):
-        sensor = GroveGSRSensor(int(sys.argv[1]))
+    def measure(self, sets):
+        """
+        sets = [set1, set2, set3]
+        """
+        if len(sets) != 3:
+            print('Number of sets must be 3')
+            sys.exit(1)
 
-        sec=0
-        if(state==0):
-            print('---CALIBRATION---')
-            for sec in range(60):
-                value = sensor.GSR
-                self.writer.writerow([sec, value, state])
-                print('SEC:{0}-GSR:{1}-STATE:{2}'.format(sec,value,state))
-                time.sleep(1)
+        sensor = GroveGSRSensor(0)
 
-        if(state==1 or state==2):
-            print('---STATE {0}---'.format(state))
-            for sec in range(180):
-                value = sensor.GSR
-                self.writer.writerow([sec, value, state])
-                print('SEC:{0}-GSR:{1}-STATE:{2}'.format(sec,value,state))
-                time.sleep(1)
-            if(state == 2):
-                self.file.close()
-                print("---FINISH---")
+        sec = 0
+        state = 0
+        for sec in range(sum(sets)):
+            if sum(sets[:state]) == sec :
+                state += 1
+                print('---{}---'.format(state))
+            value = sensor.GSR
+            self.writer.writerow([sec, value, state])
+            print('SEC:{0}-GSR:{1}-STATE:{2}'.format(sec,value,state))
+            time.sleep(1)
+
+        self.file.close()
+        print("---FINISH---")
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: {} adc_channel'.format(sys.argv[0]))
-        sys.exit(1)
 
     """
-    state 0 : calibration
-    state 1 : focus
-    state 2 : rest
+    state 1 : calibration
+    state 2 : focus
+    state 3 : rest
     """
 
+    sets = [int(i) for i in sys.argv[1:]]
+    print(sets)
     result = MeasureGSR()
-    for state in range(0,3):
-        result.measure(state)
+    result.measure(sets)
 
 if __name__ == '__main__':
     main()
