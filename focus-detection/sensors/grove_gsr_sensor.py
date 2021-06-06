840c71f9c6785e2b2d4a68f45442e70a0102c048
keisuke-jpn <keiske.message@gmail.com>
2021-06-06 18:26:30 +0300

    Separated sensor program

---
+++ focus-detection/sensors/grove_gsr_sensor.py
@@ -0,0 +1,10 @@
+from grove.adc import ADC
+class GroveGSRSensor:
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+
+    @property
+    def GSR(self):
+        value = self.adc.read(self.channel)
+        return value
