2f6e2dcfa1c82e20aede3a580f778a2cbc2e79a9
keisuke-jpn <keiske.message@gmail.com>
2021-06-06 18:19:49 +0300

    Changed constructer in __init__

--- focus-detection/gsr.py
+++ focus-detection/gsr.py
@@ -1,48 +1,42 @@
 import math
 import sys
 import time
-from grove.adc import ADC
 import csv
 import datetime
+import shutil
+from sensors.grove_gsr_sensor import GroveGSRSensor
 
-class GroveGSRSensor:
-    def __init__(self, channel):
-        self.channel = channel
-        self.adc = ADC()
-
-    @property
-    def GSR(self):
-        value = self.adc.read(self.channel)
-        return value
+TEMP_CSV_PATH = "./gsr_record.csv"
 
 class measure_gsr:
-    def __init__(self):
-        #Create CSV File
-        filename = input("Name CSV File: ")+'.csv'
-        self.file = open(filename, 'w')
-        self.writer=csv.writer(self.file,lineterminator='\n')
-        self.writer.writerow(['SEC','GSR','STATE'])
+    def __init__(self, sets, filename):
+        self.sets = sets
+        self.filename = '../csv/'+filename
 
-    def measure(self, sets):
+    def measure(self):
         """
         sets = [set1, set2, set3...]
         """
-        if len(sets) != 5:
-            print('Number of sets must be 5')
-            sys.exit(1)
-
+        #Create temporary CSV File
+        file = open(TEMP_CSV_PATH, 'w')
+        writer=csv.writer(file,lineterminator='\n')
+        writer.writerow(['SEC','GSR','STATE'])
+        
         sensor = GroveGSRSensor(0)
 
         sec = 0
         state = 0
-        for sec in range(sum(sets)):
-            if sum(sets[:state]) == sec :
+        for sec in range(sum(self.sets)):
+            if sum(self.sets[:state]) == sec :
                 state += 1
                 print('---{}---'.format(state))
             value = sensor.GSR
-            self.writer.writerow([sec, value, state])
+            writer.writerow([sec, value, state])
             print('SEC:{0}-GSR:{1}-STATE:{2}'.format(sec,value,state))
             time.sleep(0.5)
 
-        self.file.close()
+        file.close()
+        
+        #Move temporary csv to csv folder with name input
+        shutil.copy(TEMP_CSV_PATH,self.filename)
         print("---FINISH---")
