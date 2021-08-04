15b86929d124ea99a68a618a460da333ea0ab36b
keisuke-jpn <keiske.message@gmail.com>
2021-08-04 23:12:14 +0900

    Added logger

--- focus-detection/sensors/grove_gsr_sensor.py
+++ focus-detection/sensors/grove_gsr_sensor.py
@@ -1,10 +1,15 @@
 from grove.adc import ADC
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class GroveGSRSensor:
     def __init__(self, channel):
-        self.channel = channel
-        self.adc = ADC()
+        self._channel = channel
+        self._adc = ADC()
 
     @property
     def GSR(self):
-        value = self.adc.read(self.channel)
+        value = self._adc.read(self._channel)
         return value
