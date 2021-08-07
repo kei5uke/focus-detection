6d7d9b67b9cf6683fd635f16ea6dfd4d4610b30f
keisuke-jpn <keiske.message@gmail.com>
2021-08-07 22:55:51 +0900

    Changed class name. Added logger.

--- focus-detection/sensors/grove_gsr_sensor.py
+++ focus-detection/sensors/grove_gsr_sensor.py
@@ -1,15 +1,20 @@
 from grove.adc import ADC
 import logging
 
-logger = logging.getLogger(__name__)
+from logging import basicConfig, getLogger, DEBUG
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
 
-
-class GroveGSRSensor:
+class GSRSensor:
+    '''
+    Class for Grove GSR sensor
+    '''
     def __init__(self, channel):
         self._channel = channel
         self._adc = ADC()
 
     @property
     def GSR(self):
+        logger.info('[GSR]Scanning sensor data...')
         value = self._adc.read(self._channel)
         return value
