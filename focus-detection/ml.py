99ed131b4e4a279ae461e9c606af1d267b023414
keisuke-jpn <keiske.message@gmail.com>
2021-08-18 22:48:01 +0900

    Save the best model in model folder

--- focus-detection/ml.py
+++ focus-detection/ml.py
@@ -1,107 +1,106 @@
-import pandas as pd
-import numpy as np
-from scipy import stats
-
 from hyperopt import Trials, STATUS_OK, tpe
 from hyperas import optim
 from hyperas.distributions import choice
 
-import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, LSTM
 
+import pandas as pd
+import numpy as np
+import collections
 
-def create_data(X,Y):
-    time_step=120
-    x_data, y_data = [], []
+import gc
+from tensorflow.python.keras import backend as K
+K.clear_session()
+gc.collect()
 
-    for i in range(0,X.size-time_step,1):
-        #x_data
-        tmp = X.iloc[i:(i+time_step)]
-        x_data.append(tmp)
-        #y_data
-        labels = Y[i:(i+time_step)]
-        mode = stats.mode(labels)[0][0]
-        if mode == 2 or mode == 4:
-            y_data.append(1)
-        if mode == 3 or mode == 5:
-            y_data.append(0)
-        x = np.array(x_data)
-        y = np.array(y_data).reshape(-1,1)
-    return x.astype('float32'),y.astype('float32')
 
-def mean_and_std(x):
-    data = x.astype('float32')
-    mean = data.mean(axis=0)
-    data -= mean
-    std = data.std(axis=0)
-    return mean, std
-
-def data():
-    df = pd.read_csv('gsr_record.csv',header=0)
-    df = df.set_index('STATE')
-    df = df.drop([1])
-    
-    df_train = df.copy()
-    df_train = df_train.drop([4,5])
-    x_train, y_train = create_data(df_train['GSR'],df_train.index.to_list())
-    
-    df_test = df.copy()
-    df_test = df_test.drop([2,3])
-    x_test, y_test = create_data(df_test['GSR'],df_test.index.to_list())
-    
-    mean, std = mean_and_std(x_train[0])
-    x_train -= mean
-    x_train /= std
-    
-    mean, std = mean_and_std(x_test[0])
-    x_test -= mean
-    x_test /= std
-    
-    x_train = np.reshape(x_train,[x_train.shape[0],x_train.shape[1],1])
-    x_test = np.reshape(x_test,[x_test.shape[0],x_test.shape[1],1])
-    
-    return x_train, y_train, x_test, y_test
-
-def model(x_train, y_train, x_test, y_test):
+def model(x_train, y_train):
     model = Sequential()
-    model.add(LSTM({{choice([2,4,8])}}, input_shape=(x_train.shape[1],1),return_sequences=True))
-    model.add(Dropout({{uniform(0,1)}}))
+    model.add(Dense(x_train.shape[1],
+                   input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(Dropout({{uniform(0, 0.5)}}))
+    model.add(LSTM(16,
+                   return_sequences=False))
+    model.add(Dropout({{uniform(0, 0.5)}}))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['acc'])
-    model.fit(x_train,
+    model.compile(loss = 'binary_crossentropy',
+                  optimizer = 'adam',
+                  metrics = ['acc'])
+
+    history = model.fit(x_train,
               y_train,
-              epochs={{choice([8,16,32])}},
-              batch_size={{choice([16,32,64])}},
-              validation_data=(x_test,y_test),
-              shuffle=False)
-    val_loss, val_acc = model.evaluate(x_test, y_test, verbose=0)
+              epochs = {{choice([16, 32 ,64])}},
+              batch_size = {{choice([32 ,64 ,128])}},
+              validation_split = 0.1,
+              shuffle = False)
+
+    val_acc = np.amax(history.history['val_acc'])
+    print(f'Best epoch acc:{val_acc}')
+
     return {'loss': -val_acc, 'status': STATUS_OK, 'model': model}
 
-def auto_tuning():
-    #Get best_model
-    best_run, best_model = optim.minimize(model=model,
-                                          data=data,
-                                          algo=tpe.suggest,
-                                          max_evals=2,
-                                          trials=Trials(),
-                                          functions=[create_data,mean_and_std])
-    #Get mean and std
-    df = pd.read_csv('gsr_record.csv',header=0)
-    df = df.set_index('STATE')
-    df = df.drop([1,2,3])
-    x, y = create_data(df['GSR'],df.index.to_list())
-    mean, std = mean_and_std(x[0])
+def data():
+    df = pd.read_csv('../csv/tmp.csv', header=0)
+    data = df[(df['STATE'] >= 2) & (df['STATE'] <= 5)]
+    X = data[['GSR', 'Delta', 'Theta', 'Low_Alpha', 'High_Alpha', 'Low_Beta', 'High_Beta', 'Low_Gamma', 'Mid_Gamma']].values
+    mean = X.mean(axis = 0)
+    std = X.std(axis = 0)
 
-    #Model summary
-    x_train, y_train, x_test, y_test = data()
-    val_loss, val_acc = best_model.evaluate(x_test, y_test)
-    print(best_model.summary())
-    print("val_loss:",val_loss)
-    print("val_acc:",val_acc)
+    # Train data
+    data = df[(df['STATE'] >= 2) & (df['STATE'] <= 3)]
+    X = data[['GSR', 'Delta', 'Theta', 'Low_Alpha', 'High_Alpha', 'Low_Beta', 'High_Beta', 'Low_Gamma', 'Mid_Gamma']].values
+    X = (X - mean) / std
+    y = data['STATE']
+    x_train, y_train = create_data(X, y)
+
+    # Test data
+    data = df[(df['STATE'] >= 4) & (df['STATE'] <= 5)]
+    X = data[['GSR', 'Delta', 'Theta', 'Low_Alpha', 'High_Alpha', 'Low_Beta', 'High_Beta', 'Low_Gamma', 'Mid_Gamma']].values
+    X = (X - mean) / std
+    y = data['STATE']
+    x_test, y_test = create_data(X, y)
+
+    return x_train, y_train, x_test, y_test, mean, std
+
+def create_data(X, y):
+    time_step=30;
+    x_data, y_data = [], []
+
+    for i in range(0, len(X) -time_step, 1):
+        #x_data
+        tmp = X[i:(i+time_step)].tolist()
+        x_data.append(tmp)
+        #y_data
+        labels = y[i:(i+time_step)]
+        mode = collections.Counter(labels).most_common()[0][0]
+        if mode % 2 == 0:
+            y_data.append(1)
+        if mode % 2 == 1:
+            y_data.append(0)
+
+    return np.array(x_data), np.array(y_data).reshape(-1,1)
+
+def auto_tuning(filename: str):
+    best_run, best_model = optim.minimize(model=model,
+                                      data=data,
+                                      algo=tpe.suggest,
+                                      max_evals=3,
+                                      eval_space=True,
+                                      trials=Trials(),
+                                      functions=[create_data])
+
+    x_train, y_train, x_test, y_test, mean, std = data()
+    test_loss, test_acc = best_model.evaluate(x_test, y_test)
+    print(f'Test loss:{test_loss}, Test acc:{test_acc}')
+    print(f'Best Run:{best_run}')
+
+    best_model.save('../model/' + filename)
 
     return best_model, mean, std
+
+
+if __name__ == '__main__':
+    auto_tuning('test')
