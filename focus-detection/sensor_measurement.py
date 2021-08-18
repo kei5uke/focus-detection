a73b10b99c14205b84afd0b8038e2fd17cb7733e
keisuke-jpn <keiske.message@gmail.com>
2021-08-18 21:31:48 +0900

    Deleted some private variables and moved to the method args.

--- focus-detection/sensor_measurement.py
+++ focus-detection/sensor_measurement.py
@@ -2,13 +2,14 @@ from sensors.grove_gsr_sensor import GSRSensor
 from sensors.ns_eeg_sensor import EEGSensor
 import csv
 import math
-import time
+import shutil
+from time import sleep
 import numpy as np
 import concurrent.futures
 import logging
 logger = logging.getLogger(__name__)
 
-TEMP_CSV_PATH = './sensor_record.csv'
+TEMP_CSV_PATH = '../csv/tmp.csv'
 EEG_PORT = '/dev/rfcomm0'
 GSR_PORT = 1
 
@@ -17,11 +18,7 @@ class SensorMeasurement(GSRSensor, EEGSensor):
     '''
     Class for gathering sensors data
     '''
-    def __init__(self, sessions: list, filename: str, eeg_port, gsr_port, step = 0.5):
-        self.__sessions = sessions
-        self.__step = step
-        self.__filename = '../csv/' + filename
-
+    def __init__(self, eeg_port, gsr_port):
         super().__init__(gsr_port) # Inheritence of EEG
         super(GSRSensor, self).__init__(port = eeg_port) # Inheritence of GSR
 
@@ -30,7 +27,7 @@ class SensorMeasurement(GSRSensor, EEGSensor):
         logger.info('Connect to all of sensors...')
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
         executor.submit(EEGSensor.start_eeg_sensor, self)
-        #Future use: Add new sensor class and executor
+        #Future use: Add new sensor class and put it in executor
 
     def pause_sensor_connection(self):
         ''' Stop updating values temporary '''
@@ -47,43 +44,56 @@ class SensorMeasurement(GSRSensor, EEGSensor):
         logger.info('Terminate all the connection...')
         super(GSRSensor, self).terminate_eeg_sensor()
 
-    def collect_data_for_ml(self):
-        if not super(GSRSensor, self).is_working : raise RuntimeError('EEG is not working. Make connection beforehand')
-        if isinstance(super().GSR, int) == False: raise RuntimeError('GSR Sensor is not working properly.')
-        data = self.__measure_data()
+    def collect_data_for_ml(self, filename: str, sessions: list, step = 0.5):
+        if not super(GSRSensor, self).is_eeg_working:
+            raise RuntimeError('EEG is not working. Make connection beforehand')
+            self.terminate_sensor_connection()
+            sys.exit()
+
+        if isinstance(super().GSR, int) == False:
+            raise RuntimeError('GSR Sensor is not working properly.')
+            self.terminate_sensor_connection()
+            sys.exit()
+
+        data = self.__measure_data(sessions, step)
         self.pause_sensor_connection()
-        self.__output_csv(data)
+        self.__output_csv(data, filename)
         self.continue_sensor_connection()
 
-    def __measure_data(self):
+    def __measure_data(self, sessions: list, step: float):
         ''' Gather sensors data
         and return data = [[sec, state, gsr, eeg]...] '''
         logger.info('Start gathering sensors data')
         data = []
         state = 0
-        for sec in np.arange(0, sum(self.__sessions), self.__step):
-            if sum(self.__sessions[:state]) == sec:
+        for sec in np.arange(0, sum(sessions), step):
+            if sum(sessions[:state]) == sec:
                 state += 1
                 logger.info(f'---SESSION {state}---')
             row = []
             row.extend([sec, state, super().GSR])
             row.extend(super(GSRSensor, self).EEG)
             data.append(row)
-            time.sleep(self.__step)
+            sleep(step)
 
         logger.info('Finish gathering sensors data')
         return data
 
-    def __output_csv(self, data):
+    def __output_csv(self, data, filename: str):
         ''' Recieve the data and write it in csv '''
-        if data == None: raise RuntimeError('Data is empty. Not measured yet.')
+        if data == None:
+            raise RuntimeError('Data is empty. Not measured yet.')
+            self.terminate_sensor_connection()
+            sys.exit()
+
         logger.info('Output csv')
-        with open(self.__filename, 'w') as file:
+        with open(TEMP_CSV_PATH, 'w') as file:
             writer = csv.writer(file, lineterminator='\n')
             writer.writerow(['SEC', 'STATE', 'GSR','Delta','Theta','Low_Alpha',
                              'High_Alpha','Low_Beta','High_Beta','Low_Gamma','Mid_Gamma'])
             for row in data:
                 writer.writerow(row)
+        shutil.copyfile(TEMP_CSV_PATH, filename)
         logger.info('Finish outputting csv')
 
 
@@ -97,10 +107,9 @@ def main():
         logging.getLogger(module).setLevel(level=level)
 
     print(SensorMeasurement.mro())
-    sensor = SensorMeasurement(sessions = [12, 12, 12], filename = 'test.csv',
-                               eeg_port = EEG_PORT, gsr_port = GSR_PORT)
+    sensor = SensorMeasurement(eeg_port = EEG_PORT, gsr_port = GSR_PORT)
     sensor.start_sensor_connection()
-    sensor.collect_data_for_ml()
+    sensor.collect_data_for_ml(sessions = [60, 120, 120, 120, 120], filename = 'test')
     sensor.terminate_sensor_connection()
 
 if __name__ == '__main__':
