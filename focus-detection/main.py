042f14f046c5d8f6697d8965c5b31ba11b2a29d3
keisuke-jpn <keiske.message@gmail.com>
2021-08-18 22:55:39 +0900

    Complete real time classification

--- focus-detection/main.py
+++ focus-detection/main.py
@@ -1,11 +1,46 @@
-import gsr
 import ml
-from sensors.grove_gsr_sensor import GroveGSRSensor
-import time
-import copy as cp
+from sensor_measurement import SensorMeasurement
 import numpy as np
+from time import sleep
+import os
+from shutil import copyfile
+from tensorflow.keras.models import load_model
+
+import logging
+logger = logging.getLogger(__name__)
+
+EEG_PORT = '/dev/rfcomm0'
+GSR_PORT = 1
+TEMP_CSV_PATH = '../csv/tmp.csv'
+STEP = 0.5
 
 def main():
+    '''
+    This module is used for real time classification.
+    '''
+    logging.basicConfig(
+        format='[%(asctime)s] %(name)s %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    module_levels = {'sensor_measurement':logging.INFO, 'sensors': logging.INFO, __name__: logging.DEBUG}
+    for module, level in module_levels.items():
+        logging.getLogger(module).setLevel(level=level)
+
+
+    copy_filename = input("Name CSV File: ")
+
+    # If the file name already exist, proceed to load model
+    if os.path.isdir('../model/' + copy_filename):
+        choice = input('Found the same name of the file. Load the model? [y/n]:').lower()
+        if choice == 'y':
+            copyfile('../csv/' + copy_filename + '.csv', TEMP_CSV_PATH)
+            _, _, _, _, mean, std = ml.data()
+            model = load_model('../model/' + copy_filename)
+        else: choice = 'n'
+
+    sensor = SensorMeasurement(eeg_port = EEG_PORT, gsr_port = GSR_PORT)
+    sensor.start_sensor_connection()
+
 
     """
     set 1 : calibration
@@ -14,45 +49,53 @@ def main():
     set 4 : focus data for validation
     set 5 : rest data for validation
     """
-    sets = [60,120,120,120,120]
-    filename = input("Name CSV File: ")+'.csv'
-    
-    #Collect GSR data for ML
-    process_gsr = gsr.measure_gsr(sets,filename)
-    process_gsr.measure()
-    
-    #Collect the best ml model based on the recorded data
-    #and mean, std value
-    model, mean, std = ml.auto_tuning()
-    
-    "Start real time detection"
-    #record the data for 120sec
-    sensor = GroveGSRSensor(0)
+    if choice == 'n':
+        sensor.collect_data_for_ml(sessions = [60, 120, 120, 120, 120],
+                                    filename = copy_filename,
+                                    step = STEP) # Collect EEG, GSR data for ML
+        sensor.pause_sensor_connection()
+        model, mean, std = ml.auto_tuning(copy_filename) # Collect the best ml model based on the recorded data
+        sensor.continue_eeg_sensor()
+
+    #Start real time detection
     values = []
     sec = 0.0
 
-    for i in range(120):
-        value = (sensor.GSR-mean)/std
-        values.append(value)
-        print('SEC:{0}-GSR:{1}'.format(sec,value))
-        sec += 0.5
-        time.sleep(0.5)
+    for i in range(30):
+        row = []
+        row.extend([sensor.GSR])
+        row.extend(sensor.EEG)
+        values.append(row)
+        logger.info(f'{sec} sec : {row}')
+        sec += STEP
+        sleep(STEP)
 
-    gsr_values = np.array([values]).astype('float32')
-    gsr_values = np.reshape(gsr_values,[gsr_values.shape[0],gsr_values.shape[1],1])
-    
+    values = np.array(values).reshape(1, 30, 9)
+
     while(1):
-        #predict the result
-        result = model.predict(gsr_values)
-        #delete the oldest value
-        gsr_values = np.delete(gsr_values, 0, axis=1)
-        #update the new value
-        value = (sensor.GSR - mean)/std
-        gsr_values = np.insert(gsr_values, 119, [[value]], axis=1)
-        print('SEC:{0}-GSR:{1}'.format(sec,value))
-        print("FOCUS\n") if np.average(result)>0.5 else print("NOT FOCUS\n")
-        sec += 0.5
-        time.sleep(0.5)
+        try:
+            #predict the result
+            norm_value = (values - mean) / std
+            result = model.predict(norm_value)
+            logger.info("FOCUS\n") if result >= 0.5 else logger.info("NOT FOCUS\n")
+
+            #delete the oldest value
+            values = np.delete(values, 0, axis = 1)
+
+            #update the new value
+            row = []
+            row.extend([sensor.GSR])
+            row.extend(sensor.EEG)
+            values = np.insert(values, 29, row, axis = 1)
+            logger.debug(f'{sec} sec : {row}')
+
+            sec += STEP
+            sleep(STEP)
+
+        except KeyboardInterrupt:
+            logger.info('Keyboard Interrupt')
+            sensor.terminate_sensor_connection()
+            exit(0)
 
 if __name__ == '__main__':
     main()
